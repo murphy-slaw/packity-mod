e2c230bdb6aa46295d13b91059ec91810fe27482
Kevin Murphy <kevin@eorbit.net>
2022-05-11 11:47:55 -0700

    reconfigure some defaults

--- src/main/java/net/funkpla/example/mixin/ExampleMixin.java
+++ src/main/java/net/funkpla/example/mixin/ExampleMixin.java
@@ -1,6 +1,6 @@
-package net.fabricmc.example.mixin;
+package net.funkpla.example.mixin;
 
-import net.fabricmc.example.ExampleMod;
+import net.funkpla.example.ExampleMod;
 import net.minecraft.client.gui.screen.TitleScreen;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
